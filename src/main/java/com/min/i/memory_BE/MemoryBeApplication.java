a2540cab2bdbf768243ff153b8766b2d6a4e28f6
김동현 <ruggy245@naver.com>
2024-12-01 15:34:30 +0900

    feat: Initial project setup

---
+++ src/main/java/com/min/i/memory_BE/MemoryBeApplication.java
@@ -0,0 +1,13 @@
+package com.min.i.memory_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryBeApplication.class, args);
+	}
+
+}
