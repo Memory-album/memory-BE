ccb49abfe12532e72ed4e21f300dc8e825573a02
김동현 <ruggy245@naver.com>
2024-12-13 19:33:14 +0900

    feat: global error 설정

---
+++ src/main/java/com/min/i/memory_BE/global/error/ErrorMessage.java
@@ -0,0 +1,5 @@
+package com.min.i.memory_BE.global.error;
+
+public class ErrorMessage {
+
+}
---
+++ src/main/java/com/min/i/memory_BE/global/error/exception/EntityNotFoundException.java
@@ -0,0 +1,5 @@
+package com.min.i.memory_BE.global.error.exception;
+
+public class EntityNotFoundException {
+
+}
