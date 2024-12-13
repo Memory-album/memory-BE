0c55075215fef04ead87ee9f8cf820d78a1a2f14
김동현 <ruggy245@naver.com>
2024-12-13 20:05:18 +0900

    refector: 생성자 추가

--- src/main/java/com/min/i/memory_BE/global/error/exception/ApiException.java
+++ src/main/java/com/min/i/memory_BE/global/error/exception/ApiException.java
@@ -2,18 +2,15 @@ package com.min.i.memory_BE.global.error.exception;
 
 import com.min.i.memory_BE.global.error.ErrorCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class ApiException extends RuntimeException {
   private final ErrorCode errorCode;
   
   public ApiException(String message, ErrorCode errorCode) {
     super(message);
-    this.errorCode = errorCode;
-  }
-  
-  public ApiException(ErrorCode errorCode) {
-    super(errorCode.getMessage());
     this.errorCode = errorCode;
   }
 }
