6bea588c5773abf0f56c5f7ecd960df9e6db4f0c
김동현 <ruggy245@naver.com>
2025-01-02 13:38:38 +0900

    fix: add swaggerConfig update

--- src/main/java/com/min/i/memory_BE/global/config/SwaggerConfig.java
+++ src/main/java/com/min/i/memory_BE/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.min.i.memory_BE.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +17,20 @@ public class SwaggerConfig {
       .version("v1.0")
       .description("Min:i API 문서");
     
+    // Security Scheme 설정 추가
+    SecurityScheme securityScheme = new SecurityScheme()
+      .type(SecurityScheme.Type.HTTP) // HTTP 방식 인증
+      .scheme("bearer") // Bearer Token 사용
+      .bearerFormat("JWT"); // JWT 형식 지정
+    
+    // Security Requirement 추가
+    SecurityRequirement securityRequirement = new SecurityRequirement()
+      .addList("BearerAuth");
+    
     return new OpenAPI()
-      .info(info);
+      .info(info)
+      .addSecurityItem(securityRequirement) // Security Requirement 추가
+      .components(new io.swagger.v3.oas.models.Components()
+        .addSecuritySchemes("BearerAuth", securityScheme)); // Security Scheme 등록
   }
 }
