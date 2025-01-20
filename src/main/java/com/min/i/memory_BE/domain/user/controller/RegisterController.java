71c8037325bc3d114a2b45792b64137bfddf3541
우 연정 <yj753110@naver.com>
2025-01-21 02:25:14 +0900

    refactor: 사용자 정보 수정 API 통합

--- src/main/java/com/min/i/memory_BE/domain/user/controller/RegisterController.java
+++ src/main/java/com/min/i/memory_BE/domain/user/controller/RegisterController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
