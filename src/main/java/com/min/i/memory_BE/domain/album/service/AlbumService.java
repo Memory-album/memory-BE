9f9975d57025375fe1b4b73d7f98980409267ebb
summit1123 <ruggy245@naver.com>
2025-01-25 22:04:43 +0900

    feat: album controller

---
+++ src/main/java/com/min/i/memory_BE/domain/album/service/AlbumService.java
@@ -0,0 +1,35 @@
+package com.min.i.memory_BE.domain.album.service;
+
+import com.min.i.memory_BE.domain.album.repository.AlbumRepository;
+import com.min.i.memory_BE.global.service.S3Service;
+import com.min.i.memory_BE.domain.album.dto.request.AlbumRequestDto;
+import com.min.i.memory_BE.domain.album.entity.Album;
+import com.min.i.memory_BE.domain.album.enums.AlbumTheme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlbumService {
+    
+    private final AlbumRepository albumRepository;
+    private final S3Service s3Service;
+    
+    @Autowired
+    public AlbumService(AlbumRepository albumRepository, S3Service s3Service) {
+        this.albumRepository = albumRepository;
+        this.s3Service = s3Service;
+    }
+    
+    public Album createAlbum(AlbumRequestDto request) {
+        String thumbnailUrl = s3Service.uploadThumbnail(request.getThumbnailFile());
+        
+        Album album = Album.builder()
+                .title(request.getTitle())
+                .description(request.getDescription())
+                .thumbnailUrl(thumbnailUrl)
+                .theme(AlbumTheme.valueOf(request.getTheme().toUpperCase()))
+                .build();
+        
+        return albumRepository.save(album);
+    }
+}
