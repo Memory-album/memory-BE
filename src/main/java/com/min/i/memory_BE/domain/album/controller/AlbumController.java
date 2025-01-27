c5f7a69ce2ec3f91b249ade23acc003daa8883d7
summit1123 <ruggy245@naver.com>
2025-01-27 23:37:48 +0900

    style: 불필요한 주석 제거

--- src/main/java/com/min/i/memory_BE/domain/album/controller/AlbumController.java
+++ src/main/java/com/min/i/memory_BE/domain/album/controller/AlbumController.java
@@ -1,11 +1,9 @@
 package com.min.i.memory_BE.domain.album.controller;
 
 import com.min.i.memory_BE.domain.album.entity.Album;
-import com.min.i.memory_BE.domain.album.enums.AlbumTheme;
 import com.min.i.memory_BE.domain.album.service.AlbumService;
 import com.min.i.memory_BE.domain.album.dto.request.AlbumRequestDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
