7cd3d971805710275040f172000cea92e40ca19a
summit1123 <ruggy245@naver.com>
2025-02-07 03:35:59 +0900

    refactor : groupListDto description 추가

--- src/main/java/com/min/i/memory_BE/domain/group/dto/response/GroupListResponseDto.java
+++ src/main/java/com/min/i/memory_BE/domain/group/dto/response/GroupListResponseDto.java
@@ -10,12 +10,14 @@ public class GroupListResponseDto {
   private Long id;
   private String name;
   private String groupImageUrl;
+  private String description;
   
   public static GroupListResponseDto from(Group group) {
     return GroupListResponseDto.builder()
       .id(group.getId())
       .name(group.getName())
       .groupImageUrl(group.getGroupImageUrl())
+      .description(group.getGroupDescription())
       .build();
   }
 }
