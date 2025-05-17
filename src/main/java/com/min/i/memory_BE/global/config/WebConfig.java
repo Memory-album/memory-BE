e54a0ca659911bc63749167ce2f866a5207ea66b
summit1123 <ruggy245@naver.com>
2025-05-18 00:58:01 +0900

    refactor : 도메인 추가

--- src/main/java/com/min/i/memory_BE/global/config/WebConfig.java
+++ src/main/java/com/min/i/memory_BE/global/config/WebConfig.java
@@ -9,32 +9,32 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-      .allowedOrigins("http://localhost:3000")  // React 개발 서버
+      .allowedOrigins("http://localhost:3000","https://min-21h87evi1-yeonguks-projects.vercel.app","https://min-i.vercel.app")  // React 개발 서버
       .allowedMethods("*")
       .allowedHeaders("*");
 
     // 추가: /user/** 경로에도 CORS 허용
     registry.addMapping("/user/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000","https://min-21h87evi1-yeonguks-projects.vercel.app","https://min-i.vercel.app")
             .allowedMethods("*")
             .allowCredentials(true) // 쿠키 포함 요청 허용
             .allowedHeaders("*");
 
     registry.addMapping("/register/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000","https://min-21h87evi1-yeonguks-projects.vercel.app","https://min-i.vercel.app")
             .allowedMethods("*")
             .allowCredentials(true) // 쿠키 포함 요청 허용
             .allowedHeaders("*");
 
     registry.addMapping("/auth/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000","https://min-21h87evi1-yeonguks-projects.vercel.app","https://min-i.vercel.app")
             .allowedMethods("*")
             .allowCredentials(true) // 쿠키 포함 요청 허용
             .allowedHeaders("*");
 
     // 추가: /user/** 경로에도 CORS 허용
     registry.addMapping("/oauth/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000","https://min-21h87evi1-yeonguks-projects.vercel.app","https://min-i.vercel.app")
             .allowedMethods("*")
             .allowedHeaders("*");
   }
